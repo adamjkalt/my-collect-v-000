c0bcea51aa61197bb8468d0a42f3592120b2c17d
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-13 18:27:43 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ def my_collect(array)
   i = 0
   while i < array.length
     #binding.pry
-    collection << yield(array[i].upcase)
+    collection << yield(array[i])
     i += 1
   end
   collection
